6515b34ec3f49849f3f0ad80a71bf2ebf63badf1
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-20 15:28:14 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
         // Skriv en extern jämförare som jämför objekt av klassen Card
         //  - Endast valörerna spelar roll
 
+        CardComp cc = new CardComp();
 
+        Card c1 = new Card();
+        c1.rank = 6;
+
+        Card c2 = new Card();
+        c2.rank = 6;
+
+        System.out.println(cc.compare(c1, c2));
+    }
+}
+
+class CardComp implements Comparator<Card> {
+
+    @Override
+    public int compare(Card c1, Card c2) {
+        if (c1.rank == c2.rank) {
+            return 0;
+        } else if (c2.rank == 1) {
+            return -1;
+        } else if (c1.rank == 1 || c1.rank > c2.rank) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 }
 
